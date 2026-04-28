0b98d77fab75a837ef83ca83d976fea8f01c53aa
Guilherme <joaoguilherme.aa1603@gmail.com>
2026-04-28 16:58:10 -0300

    chore(frontend): make yarn test run all frontend test suites

--- front-end/Sophena/e2e/vue.spec.ts
+++ front-end/Sophena/e2e/vue.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from '@playwright/test'
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
-  await page.goto('/')
-  await expect(page.locator('h1')).toHaveText('You did it!')
+test('abre a tela de login em português', async ({ page }) => {
+  await page.goto('/login')
+
+  await expect(page.getByRole('heading', { name: 'Entrar' })).toBeVisible()
+  await expect(page.getByLabel('Usuário')).toBeVisible()
+  await expect(page.getByLabel('Senha')).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Entrar' })).toBeVisible()
 })
